2e0532c67d15060b015000b144997d5f86105978
Víctor González Carro <victorgonzaleztr22@gmail.com>
2022-05-13 12:14:39 +0200

    index security

---
+++ ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.salesianostriana.dam.proyectofinal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping("/inicio")
+    public String login() {
+        return "inicio.html";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "index.html";
+    }
+
+}
---
+++ ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/model/ClaseGym.java
@@ -0,0 +1,50 @@
+package com.salesianostriana.dam.proyectofinal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class ClaseGym {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String nombreClase;
+	private double precio;
+	private String horario; //Ej:12:30-13:30
+	private int plazas;
+	private String imgClase;
+	
+	@Builder.Default
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	@OneToMany(mappedBy = "clase", fetch = FetchType.EAGER)
+	private List <ReservaClase> reservas = new ArrayList();
+	
+	public ClaseGym(String nombreClase, double precio, String horario, int plazas, String imgClase) {
+		this.nombreClase = nombreClase;
+		this.precio = precio;
+		this.horario = horario;
+		this.plazas = plazas;
+		this.imgClase = imgClase;
+	}
+}
