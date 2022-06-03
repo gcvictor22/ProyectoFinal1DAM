12c9c229b581c705d3ec6a3a50730f5fb21751a4
Víctor González Carro <victorgonzaleztr22@gmail.com>
2022-06-03 12:22:37 +0200

    Sigue sin funcionar el login

--- ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/security/SecurityConfig.java
+++ ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/security/SecurityConfig.java
@@ -7,9 +7,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,14 +18,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final UserDetailsService userDetailsService;
+	
     @Autowired
     private UsuarioRepo usuarios;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		
+	}
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -38,9 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().loginPage("/").loginProcessingUrl("/login").defaultSuccessUrl("/inicio").failureUrl("/login-error").permitAll()
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll()
                 .and().headers().frameOptions().disable();
-
+    
     }
 
+    /*
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -64,4 +73,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
+    */
 }
