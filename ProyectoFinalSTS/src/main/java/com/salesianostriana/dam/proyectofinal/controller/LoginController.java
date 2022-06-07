3c204a111e0019efca7406d498f480c2eb488408
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-06-08 00:57:16 +0200

    Tarjetas usuario

--- ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/controller/LoginController.java
+++ ProyectoFinalSTS/src/main/java/com/salesianostriana/dam/proyectofinal/controller/LoginController.java
@@ -1,20 +1,26 @@
 package com.salesianostriana.dam.proyectofinal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.salesianostriana.dam.proyectofinal.model.Usuario2;
+import com.salesianostriana.dam.proyectofinal.servicios.ClaseGymServicio;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private ClaseGymServicio claseServicio;
 	
     @GetMapping("/inicio")
     public String login(Model model, @AuthenticationPrincipal Usuario2 usuario) {
     	model.addAttribute("nombreUsuario", usuario.getNombre());
     	model.addAttribute("ap1", " "+usuario.getApellido1()+" ");
     	model.addAttribute("ap2", usuario.getApellido2());
+		model.addAttribute("lista", claseServicio.findAll());
     	return "inicio";
     }
 
